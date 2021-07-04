fea95f01e84abc6354f6f1b135f9f8416507c5c3
John-McClane <37442959+John-McClane@users.noreply.github.com>
2021-07-04 22:36:35 +0300

    Add files via upload

--- js/wpxapi_link_click_log.js
+++ js/wpxapi_link_click_log.js
@@ -1,31 +1,29 @@
 jQuery(document).ready( function($) {
+	var debug = 1;
+	if (debug) {
+		$(window).ajaxComplete(function () {console.log('Ajax Complete'); });
+		$(window).ajaxError(function (data, textStatus, jqXHR) {console.log('Ajax Error');
+			console.log('data: ' + data);
+			console.log('textStatus: ' + textStatus);
+			console.log('jqXHR: ' + jqXHR); });
+		$(window).ajaxSend(function () {console.log('Ajax Send'); });
+		$(window).ajaxStart(function () {console.log('Ajax Start'); });
+		$(window).ajaxStop(function () {console.log('Ajax Stop'); });
+		$(window).ajaxSuccess(function () {console.log('Ajax Success'); });
+	}
+
+	// Let's disable the inputs for the duration of the Ajax request.
+	// Note: we disable elements AFTER the form data has been serialized.
+	// Disabled form elements will not be serialized.
+	// $inputs.prop("disabled", true);
+
 	$('a').click(function() {
-		var debug = 1;
 		var wpxapi_click_url_requested =  $(this).attr('href');
 		var wpxapi_click_referrer_location = document.location.href;
-
-		var playcount, playfrom, position, duration, pausestatement = 0;
-		var videourl = window.location.pathname;
-		var filename = videourl.substring(videourl.lastIndexOf('/')+1, videourl.lastIndexOf('.'));
-
-		// 	We're not going to sent a statement on PLAY for this example.  Instead, we're going to set the variable for what point within the video the user started playing a given period
-		//	This section shows the SEGMENT that the video played.  This is why we don't send the statement on the actual PLAY of the video, but when play stops.  This way we can gather both the start and stop points more easily
-		//  onPlay
-		jwplayer().onPlay(function(event){
-			playfrom = jwplayer().getPosition();
-			if (debug) { console.log("Video Played") };
-		});
-		//  onPause
-		jwplayer().onPause(function(event){
-			position = jwplayer().getPosition();
-			duration = jwplayer().getDuration();
-			pausestatement = 1;
-			if (debug) { console.log("Video Paused. Played from " + playfrom + " seconds, to " + position + " seconds, full duration: " + duration + " seconds. PauseStatement = " + pausestatement); }
-		});
-		//  end onPlay
-		//  Send statement when someone pauses the video
-
-		data = {
+		var jw_playcount, jw_playfrom, jw_position, jw_duration, jw_pausestatement = 0;
+		var jw_videourl = window.location.pathname;
+		var jw_filename = jw_videourl.substring(jw_videourl.lastIndexOf('/')+1, jw_videourl.lastIndexOf('.'));
+		var data = {
 			action: "xapiclicklog_action",
 			wpxapi_click_log: "true",
 			wpxapi_nonce: wpxapi_ajax_object.wpxapi_nonce,
@@ -33,50 +31,72 @@ jQuery(document).ready( function($) {
 			wpxapi_blogid: wpxapi_ajax_object.wpxapi_blogid,
 			wpxapi_click_url_requested: wpxapi_click_url_requested,
 			wpxapi_click_referrer_location: wpxapi_click_referrer_location,
-			playcount,
-			playfrom,
-			videourl,
-			filename,
-			position,
-			duration,
-			pausestatement
+			jw_playcount: wpxapi_ajax_object.jw_playcount,
+			jw_playfrom: wpxapi_ajax_object.jw_playfrom,
+			jw_videourl: wpxapi_ajax_object.jw_videourl,
+			jw_filename: wpxapi_ajax_object.jw_filename,
+			jw_position: wpxapi_ajax_object.jw_position,
+			jw_duration: wpxapi_ajax_object.jw_duration,
+			jw_pausestatement: wpxapi_ajax_object.jw_pausestatement
 		};
 
-/* 		var data = {
-			action: "xapiclicklog_action",
-			wpxapi_click_log: "true",
-			wpxapi_nonce: wpxapi_ajax_object.wpxapi_nonce,
-			wpxapi_uid: wpxapi_ajax_object.wpxapi_uid,
-			wpxapi_blogid: wpxapi_ajax_object.wpxapi_blogid,
-			wpxapi_click_url_requested: wpxapi_click_url_requested,
-			wpxapi_click_referrer_location: wpxapi_click_referrer_location
-		}; */
-
-/* 		if (debug) { console.log( "Data to be transmitted: action: " + action + ", wpxapi_click_log: " + wpxapi_click_log + ", wpxapi_nonce: " + wpxapi_nonce + ", wpxapi_uid: " + wpxapi_uid + ", wpxapi_blogid: " + wpxapi_blogid + ", wpxapi_click_url_requested: " + wpxapi_click_url_requested + ", wpxapi_click_referrer_location: " + wpxapi_click_referrer_location + ", wpxapi_playcount: " + wpxapi_playcount + ", wpxapi_playfrom: " + wpxapi_playfrom + ", wpxapi_videourl: " + wpxapi_videourl + ", wpxapi_filename: " + wpxapi_filename + ", wpxapi_duration: " + wpxapi_duration + ", wpxapi_position: " + wpxapi_position + ", wpxapi_pausestatement: " + wpxapi_pausestatement ); } */
-
-		if (debug) { console.log( "Data to be transmitted: playcount: " + playcount + ", playfrom: " + playfrom + ", videourl: " + videourl + ", filename: " + filename + ", duration: " + duration + ", position: " + position + ", pausestatement: " + pausestatement ); }
-
-		$.post( wpxapi_ajax_object.ajax_url, data, function( response ) {
-			// alert( 'Got this response from the server: ' + response );
+		// 	We're not going to sent a statement on PLAY for this example.  Instead, we're going to set the variable for what point within the video the user started playing a given period
+		//	This section shows the SEGMENT that the video played.  This is why we don't send the statement on the actual PLAY of the video, but when play stops.  This way we can gather both the start and stop points more easily
+		//  onPlay
+		jwplayer().onPlay(function(event){
+			jw_playfrom = jwplayer().getPosition();
 			if (debug) {
-				console.log( "Data transmitted to server." );
+				console.log("Video Played from timestamp: " + jw_playfrom);
+			}
+		});
+		//  onPause
+		jwplayer().onPause(function(event){
+			jw_position = jwplayer().getPosition();
+			jw_duration = jwplayer().getDuration();
+			jw_pausestatement = 1;
+			if (debug) {
+				console.log("Video Paused. Played from " + jw_playfrom + " seconds, to " + jw_position + " seconds, full video duration: " + jw_duration + " seconds. PauseStatement = " + jw_pausestatement);
+			}
+		});
+		//  end onPlay
+		//  Send statement when someone pauses the video
+
+		if (debug) {
+			console.log( "Data to be transmitted: " + data );
+		}
+
+		// request =
+		$.post( wpxapi_ajax_object.ajax_url, data, function( response ) {
+			if (debug) {
+				alert('Got this response from the server: ' + response );
+				console.log( "Data transmitted to server: " + data );
 				console.log( "Got this response from the server: " + response );
 			}
 			// die();
 		});
+
+		// if (debug) {
+		// 	// Callback handler that will be called on success
+		// 	request.done(function (response, textStatus, jqXHR){
+		// 		// Log a message to the console
+		// 		alert('Successfully called');
+		// 		console.log("Success, it worked!");
+		// 		console.log("Success. Data: " + data);
+		// 	});
+
+		// 	// Callback handler that will be called on failure
+		// 	request.fail(function (jqXHR, textStatus, errorThrown){
+		// 		// Log the error to the console
+		// 		alert('Error: ', errorThrown);
+		// 		console.error("The following error occurred: "+ textStatus, errorThrown);
+		// 		console.log("Error. Data: " + data + jqXHR);
+		// 	});
+
+		// 	// Callback handler that will be called regardless if the request failed or succeeded
+		// 	request.always(function () {
+		// 		// Reenable the inputs
+		// 		// $inputs.prop("disabled", false);
+		// 	});
+		// }
 	});
 });
-
-/* 	$.ajax({
-		type:"POST",
-		url: "/includes/triggers.php",
-		data: {var1: 'playcount', var2: 'playfrom', var3: 'videourl', var4: 'filename', var5: 'duration', var6: 'position'},
-		success: function(){
-			if ( $debug ) {
-				//do stuff after the AJAX Calls successfully completes
-				alert("AJAX Calls Submitted");
-				console.log("AJAX Calls Submitted");
-			}
-		}
-	});
-}); */
