928a66fc83076e665ab4f415bd79751d2abb3cb3
John-McClane <37442959+John-McClane@users.noreply.github.com>
2024-09-30 16:14:47 +0300

    Add files via upload

--- KMeans_Clustering_on_Learning_Analytics_Data/MultiProcessWebScraping.py
+++ KMeans_Clustering_on_Learning_Analytics_Data/MultiProcessWebScraping.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+import os.path
 import random
 import time
 from datetime import datetime
 from multiprocessing import Process
+from fake_useragent import UserAgent
 
 verboselogs = 1
 headless = 1
@@ -16,10 +19,14 @@ headless = 1
 def create_browser():
     # Set UserAgent
     # The list of UserAgents to rotate on
-    UserAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:129.0) Gecko/20100101 Firefox/131.0" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:128.0) Gecko/20100101 Firefox/130.0" , "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"]
+    UserAgents = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:131.0) Gecko/20100101 Firefox/131.0" , "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:130.0) Gecko/20100101 Firefox/130.0" , "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36" , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36" , "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"]
 
     # Set up Chrome options
     chrome_options = Options()
+    homedir = os.path.expanduser("~")
+    chrome_options.binary_location = f"{homedir}/chrome-linux64/chrome"
+    webdriver_service = Service(f"{homedir}/chromedriver-linux64/chromedriver")
+
     if headless:
         chrome_options.add_argument("--headless")  # Run in headless mode for faster performance
         chrome_options.add_argument("--window-size=1920,1080")
@@ -27,12 +34,16 @@ def create_browser():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
 
+    # ua = UserAgent()
+    # create_browser.UserAgent = ua.random
+
     # create_browser.UserAgent = random.choices(UserAgents, weights=(10, 10, 7, 7, 3, 3, 5, 5), k=1)
     create_browser.UserAgent = random.choices(UserAgents, weights=(1, 1, 1, 1, 1, 1, 1, 1), k=1)
     chrome_options.add_argument(f"--user-agent={create_browser.UserAgent}")
 
     # Create a new instance of Chrome WebDriver
-    browser = webdriver.Chrome(options=chrome_options)
+    # browser = webdriver.Chrome(options=chrome_options)
+    browser = webdriver.Chrome(service=webdriver_service, options=chrome_options)
     return browser
 
 def send_keys_to_page(url, key):
@@ -107,21 +118,20 @@ def send_keys_to_page(url, key):
     finally:
         browser.quit()                                  # Close the browser
 
-
 if __name__ == "__main__":
     # Define the URLs and keys to send
     # ints = [random.randint(0,9)]
     pages = [
-        {"url": "example.com", "key": "1"},
-        {"url": "example.com", "key": "2"},
-        {"url": "example.com", "key": "3"},
-        {"url": "example.com", "key": "4"},
-        {"url": "example.com", "key": "5"},
-        {"url": "example.com", "key": "6"},
-        {"url": "example.com", "key": "7"},
-        {"url": "example.com", "key": "8"},
-        {"url": "example.com", "key": "9"},
-        {"url": "example.com", "key": "10"}
+        {"url": "https://example.com", "key": "1"},
+        {"url": "https://example.com", "key": "2"},
+        {"url": "https://example.com", "key": "3"},
+        {"url": "https://example.com", "key": "4"},
+        {"url": "https://example.com", "key": "5"},
+        {"url": "https://example.com", "key": "6"},
+        {"url": "https://example.com", "key": "7"},
+        {"url": "https://example.com", "key": "8"},
+        {"url": "https://example.com", "key": "9"},
+        {"url": "https://example.com", "key": "10"}
     ]
 
     processes = []
